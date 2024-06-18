4347ed1967449946fa538704c1ad0568d1984985
Yeshiv sahu <yeshivsahuofficial@gmail.com>
2024-06-19 00:38:45 +0530

    First Python Program

---
+++ helloPython.py
@@ -0,0 +1,13 @@
+print("Hello Python")
+print("Hi my name is ","Yeshiv sahu")
+
+#For next line in Python we write EMPTy print function:
+print()
+
+# "end" removes the whitespaces after the statement
+print("Hello Python ",end="")
+print("Hi my name is ","Yeshiv sahu")
+
+ 
+
+
