6f5bf9c75f575a7be5c9204fe7d475885e9c0b7c
Yeshiv sahu <yeshivsahuofficial@gmail.com>
2024-06-20 10:42:16 +0530

    Converts number(ASCII) to 'string'.

---
+++ Strings/Strings.py
@@ -0,0 +1,22 @@
+#Strings are indexed
+str1="Yeshiv Sahu"
+print(str1)
+
+#Length function
+print(len(str1))
+
+# 'in' function
+print("shiv" in str1)
+
+#Accessing element via index number
+print(str1[0],end="")
+print(str1[1],end="")
+print(str1[6],end="")
+print(str1[7])
+
+#Slicing
+print(str1[0:6])    #Yeshiv
+print(str1[5:8])
+print(str1[::-1])    #Reverse
+
+
---
+++ chr()_function.py
@@ -0,0 +1,7 @@
+x=65    #ASCII value of 'A'
+print(chr(x))
+print(chr(97))
+print(chr(78))
+print(chr(0))
+print(chr(9))
+
