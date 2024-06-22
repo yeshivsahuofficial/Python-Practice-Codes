3fbe702a1bfac4aa20ef5291183afc6d35898dbf
Yeshiv sahu <yeshivsahuofficial@gmail.com>
2024-06-22 22:43:36 +0530

    Iteration of characters from String

---
+++ Strings/String Iteration.py
@@ -0,0 +1,10 @@
+name="Yeshiv Sahu"
+
+#Using simple 'for' loop
+for c in name:
+    print(c,end="")
+
+#Using 'for' loop along with range
+print()
+for c in range(len(name)):
+    print(name[c],end="")
