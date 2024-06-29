db7ac45aca42df92805787824dced46d20a8a578
Yeshiv sahu <yeshivsahuofficial@gmail.com>
2024-06-29 16:04:24 +0530

    Pyhton Modules

---
+++ Modules/Arithmetic_module.py
@@ -0,0 +1,7 @@
+def add(a,b):
+    c=a+b
+    return c
+
+def mul(a,b):
+    c=a*b
+    return c
---
+++ Modules/Artithemetic module test.py
@@ -0,0 +1,6 @@
+import Arithmetic_module
+
+print(Arithmetic_module.add(34,5))
+
+print(Arithmetic_module.mul(4,5))
+
