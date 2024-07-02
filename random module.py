8c7a5eae4892e26b019cf8d8454d9ce1c5dc3ae8
Yeshiv sahu <yeshivsahuofficial@gmail.com>
2024-07-02 10:04:05 +0530

    Random module

---
+++ random module.py
@@ -0,0 +1,7 @@
+import random
+print(random.randint(1,5))  #includes 1 & 5 both
+
+print(random.randrange(1,5))    #does not include 5
+
+print(random.choice(["a","b","c","d",6])) #prints random element from list
+
